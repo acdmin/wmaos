704d491a2f39c3142e32d830120e64aa006f3924
wangms <wangms@qiu.com>
2015-03-31 16:30:09 +0800

    2015-03-31

---
+++ index.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="keywords" content="个人,主页,个人主页,简历,招聘,企业招聘,人才,求职,作品展示"/>
+<meta name="description" content="天生我材必有用,千金散尽还复来.是金子总会发光"/>
+<title>个人,主页,个人主页,简历,招聘,企业招聘,人才,求职,作品展示</title>
+<link href="css/reset.css" rel="stylesheet" charset="utf-8">
+<link href="css/layout.css" rel="stylesheet" charset="utf-8">
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+<script type="text/jscript" src="js/jquery.js"></script>
+</head>
+<body>
+
+<div class="section header clear">
+    <div class="sticker clear"><img src="images/sticker.gif" alt="头像" title=""></div>
+    <div class="text">
+        <p>前端设计师 @ 江西财经大学</p>
+        <p>Web Designer</p>
+    </div>
+</div>
+
+<div class="section self_introduction clear">
+	<div class="container pb60">
+    	<div class="tag"><img src="images/self_introduction.png" alt="自我介绍"></div>
+     	<div class="icon_arrow"></div>
+        <div class="my_info clear">
+        	<ul>
+            	<li><span>姓 名：</span>Mr. Mao</li>
+                <li><span>籍 贯：</span>江 西</li>
+                <li><span>性 别：</span>男</li>
+                <li><span>政治面貌：</span>群 众</li>
+                <li><span>民 族：</span>汉</li>
+                <li><span>电 话：</span>暂不公开</li>
+                <li><span>毕业院校：</span>江西财经大学</li>
+                <li><span>邮 箱：</span>605677545@qq.com</li>
+                <li><span>座右铭：</span>有志者事竟成</li>
+                <li><span>职 业：</span>前端设计师</li>
+                <li><span>自我评价：</span>Bug并不可怕，可怕的是没有信心……</li>
+            </ul>
+		</div>
+    </div>
+</div>
+
+<div class="section occupation_skill clear">
+	<div class="container pb60">
+    	<div class="tag"><img src="images/occupation_skill.png" alt="职业技能"></div>
+        <div class="icon_arrow"></div>
+        <ul class="skills clear">
+        	<li>
+            	<span>HTML</span>
+            	<div class="blood"><div class="inner" style="width:98%"></div></div>
+            </li>
+            <li>
+            	<span>HTML5</span><div class="blood"><div class="inner" style="width:80%"></div></div>
+            </li>
+            <li>
+            	<span>CSS</span><div class="blood"><div class="inner" style="width:98%"></div></div>
+            </li>
+            <li>
+            	<span>CSS3</span><div class="blood"><div class="inner" style="width:80%"></div></div>
+            </li>
+            <li>
+            	<span>JAVASCRIPT</span><div class="blood"><div class="inner" style="width:90%"></div></div>
+            </li>
+            <li>
+            	<span>PHOTOSHOP</span><div class="blood"><div class="inner" style="width:80%"></div></div>
+            </li>
+            <li>
+            	<span>FLASH</span><div class="blood"><div class="inner" style="width:20%"></div></div>
+            </li>
+            <li>
+            	<span>PHP</span><div class="blood"><div class="inner" style="width:60%"></div></div>
+            </li>
+            <li>
+            	<span>LARAVEL</span><div class="blood"><div class="inner" style="width:40%"></div></div>
+            </li>
+            <li>
+            	<span>MYSQL</span><div class="blood"><div class="inner" style="width:45%"></div></div>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="section work_experience clear">
+	<div class="container pb60">
+    	<div class="tag"><img src="images/work_experience.png" alt="工作经验"></div>
+        <div class="icon_arrow"></div>
+        <div class="clear pt60"></div>
+        <div class="work_box">
+        	<div class="work_tag">
+            	<i>-</i>
+                <em></em>
+            </div>
+        	<div class="work_tag">
+            	<label>2011.7</label>
+                <p>江西财经大学</p>
+            	<i>-</i>
+                <em></em>
+            </div>
+            <div class="work_tag">
+            	<label>2011-2012</label>
+                <p>厦门新科技</p>
+            	<i>-</i>
+                <em></em>
+            </div>
+            <div class="work_tag">
+            	<label>2012-2013</label>
+                <p>厦门都飞科技</p>
+            	<i>-</i>
+                <em></em>
+            </div>
+            <div class="work_tag">
+            	<label>2013-至今</label>
+                <p>厦门优选传媒</p>
+            	<i>-</i>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section works clear">
+	<div class="container pb20">
+    	<div class="tag"><img src="images/works.png" alt="作品集"></div>
+        <div class="icon_arrow"></div>
+        <div class="work_type clear pt60">
+        	<div class="title mt20"><i></i>一APP类<a href="javascript:;" class="more">更多>></a></div>
+            <ul class="app_list worklist">
+                <li><img src="images/works/全名枪宝石活动.jpg" alt="全名枪宝石活动"></li>
+                <li><img src="images/works/app2.jpg" alt=""></li>
+                <li><img src="images/works/app3.jpg" alt=""></li>
+                <li><img src="images/works/伴我游.jpg" alt="伴我游"></li>
+                <li><img src="images/works/app5.jpg" alt=""></li>
+                <li><img src="images/works/全名游戏.jpg" alt="全名游戏"></li>
+                <li><img src="images/works/AB配对活动.jpg" alt="AB配对活动"></li>
+                <li><img src="images/works/super_star.jpg" alt="super_star"></li>
+                <li><img src="images/works/伴我游.jpg" alt="伴我游"></li>
+                <li><img src="images/works/伴我游-登录.jpg" alt="伴我游-登录"></li>
+                <li><img src="images/works/滴滴打车-元旦.jpg" alt="滴滴打车-元旦"></li>
+                <li><img src="images/works/地图接入.jpg" alt="地图接入"></li>
+                <li><img src="images/works/勾搭一下-app.jpg" alt="勾搭一下-app"></li>
+                <li><img src="images/works/刮刮乐活动.jpg" alt="刮刮乐活动"></li>
+                <li><img src="images/works/官网APP内嵌.jpg" alt="官网APP内嵌"></li>
+                <li><img src="images/works/光棍脱单活动.jpg" alt="光棍脱单活动"></li>
+                <li><img src="images/works/韩娱明星排行.jpg" alt="韩娱明星排行"></li>
+                <li><img src="images/works/话题吹吹.jpg" alt="话题吹吹"></li>
+                <li><img src="images/works/开宝箱-1.jpg" alt="开宝箱-1"></li>
+                <li><img src="images/works/开宝箱-2.jpg" alt="开宝箱-2"></li>
+                <li><img src="images/works/客户端.jpg" alt="客户端"></li>
+                <li><img src="images/works/女神联盟.jpg" alt="女神联盟"></li>
+                <li><img src="images/works/朋友app1.jpg" alt="朋友app1"></li>
+                <li><img src="images/works/朋友app2.jpg" alt="朋友app2"></li>
+                <li><img src="images/works/拼人品抢宝石活动.jpg" alt="拼人品抢宝石活动"></li>
+                <li><img src="images/works/平安银行-点灯活动.jpg" alt="平安银行-点灯活动"></li>
+                <li><img src="images/works/全名疯狂游戏.jpg" alt="全名疯狂游戏"></li>
+                <li><img src="images/works/圣诞节-礼物.jpg" alt="圣诞节-礼物"></li>
+                <li><img src="images/works/世界杯.jpg" alt="世界杯"></li>
+                <li><img src="images/works/贪吃蛇.jpg" alt="贪吃蛇"></li>
+                <li><img src="images/works/微信游戏列表.jpg" alt="微信游戏列表"></li>
+                <li><img src="images/works/寻找有缘人.jpg" alt="寻找有缘人"></li>
+            </ul>
+        </div>
+        <div class="work_type clear">
+        	<div class="title"><i></i>一web类<a href="javascript:;" class="more">更多>></a></div>
+            <ul class="web_list worklist">
+                <li><em></em><img src="images/works/web1.jpg" alt="网商群"></li>
+                <li><img src="images/works/web2.jpg" alt=""></li>
+                <li><img src="images/works/web3.jpg" alt="球天下APP下载"></li>
+                <li><img src="images/works/新科技软件官网.jpg" alt="新科技软件官网"></li>
+                <li><img src="images/works/易城商城.jpg" alt="易城商城"></li>
+                <li><img src="images/works/世界杯2.jpg" alt="世界杯2"></li>
+                <li><img src="images/works/星树官网改版.jpg" alt="星树官网改版"></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="showImg"><img src="" alt="" id="showImg" width="auto" height="auto"></div>
+<script>
+$(function(){
+	var oLi = $('.app_list li');
+	for(var i=1;i<oLi.length;i++){
+		if(i%4 == 3){
+			$(oLi[i]).css('margin-right','0');
+		}
+	}
+	
+	var oImgLay = $('.showImg');
+	var oImg = $('#showImg')
+	$('.worklist li').click(function(){
+		var oSrc = $(this).find('img').attr('src');
+		$('.showImg').show();
+		oImg.attr('src',oSrc);
+		oImgLay.css('height',oImg.height()+'px');
+		//alert(oImg.height())
+	});
+	
+	$('.showImg').click(function(){
+		$(this).hide();
+	});
+});
+</script>
+<div class="footer clear">
+	<div class="container">
+        <h3>版权所有 @ wmaos.com</h3>
+        <p class="warm">使用IE9及以上版本浏览器访问效果更佳</p>
+        <ul class="contact_way">
+            <li class="forqq">605677545</li>
+            <li class="forphone">暂不公开</li>
+            <li class="foremail">605677545@qq.com</li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
